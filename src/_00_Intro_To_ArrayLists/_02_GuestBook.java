63d26a0af7c81ad809d32d07941118b59d8ae176
league <league@imac-18.attlocal.net>
2024-07-05 18:30:48 -0700

    finish murder of crows two

--- src/_00_Intro_To_ArrayLists/_02_GuestBook.java
+++ src/_00_Intro_To_ArrayLists/_02_GuestBook.java
@@ -1,6 +1,16 @@
 package _00_Intro_To_ArrayLists;
 
-public class _02_GuestBook {
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_GuestBook implements ActionListener {
     /*
      * Create a GUI with two buttons. One button reads "Add Name" and the other
      * button reads "View Names". When the add name button is clicked, display
@@ -13,5 +23,60 @@ public class _02_GuestBook {
      * Guest #3: Greg Ganders
      * Guest #4: Donny Doners
      */
+	
+	
+	
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JButton button1 = new JButton();
+		JButton button2 = new JButton();
+		ArrayList<String> names = new ArrayList();
+		
+		
+	
+		
+	
+	public static void main(String[] args) {
+		_02_GuestBook guestbook = new _02_GuestBook();
+		guestbook.run();
+	}
+	
+	
+	void run() {
+		panel.setPreferredSize(new Dimension(400,400));
+		frame.add(panel);
+		frame.pack();
+		button1.setText("Add Name");
+		button2.setText("View Names");
+		panel.add(button1);
+		panel.add(button2);
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+		
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(button1 == e.getSource()) {
+			 String name = JOptionPane.showInputDialog(null, "What name do you want to add");
+			 names.add(name);
+		}
+		if(button2 == e.getSource()) {
+			String theNames = "";
+			for(int i = 0; i < names.size(); i++) {
+				theNames += "Guest " + (i+1) + ":" + names.get(i) + "\n";
+				
+			}
+			JOptionPane.showMessageDialog(null, "Your guest's names are \n" + theNames);
+			
+		}
+		
+	}
+
 
 }
--- src/_01_MurderOfCrows/MurderOfCrows.java
+++ src/_01_MurderOfCrows/MurderOfCrows.java
@@ -6,18 +6,34 @@ import java.util.Random;
 public class MurderOfCrows {
 
     ArrayList<Crow> theMurder = new ArrayList<Crow>();
-
+    boolean diamondPresent = false;
     public static void main(String[] args) {
         MurderOfCrows murderOfCrows = new MurderOfCrows();
         murderOfCrows.initializeCrows();
         murderOfCrows.findTheDiamond();
+       
     }
 
     private void findTheDiamond() {
         // 1. One of the Crows has eaten the diamond. You need to search
         //    through the stomach of each Crow, then print the name of the
         //    guilty Crow.
-
+    	for(int i = 0; i < theMurder.size(); i++) {
+    		for(int j = 0; j < theMurder.get(i).getStomachContents().size(); j++ ) {
+    			if(theMurder.get(i).getStomachContents().get(j) .equals("diamond")) {
+    				diamondPresent = true;
+    				System.out.println(theMurder.get(i).getName());
+    				break;
+    			}
+    			
+    			
+    	}
+    		if(diamondPresent) {
+    			
+    			break;
+    		}
+    		
+    	}
         // 2. How many innocent crows had to die before the diamond was found?
 
     }
